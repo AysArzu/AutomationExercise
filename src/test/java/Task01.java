222a5e33873c2abc3e31f8b72694c0cf3df4ef5d
Ayse Arzu <aysebattal@gmail.com>
2023-04-06 15:40:15 +0300

    06

---
+++ src/test/java/Task01.java
@@ -0,0 +1,83 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
+
+import static org.junit.Assert.*;
+
+public class Task01 extends TestBase {
+    //Test Case 1: Register User
+    @Test
+    public void registerUser() {
+
+        //2. Navigate to url 'http://automationexercise.com'
+        driver.get("http://automationexercise.com");
+
+        //3. Verify that home page is visible successfully
+        String title = driver.getTitle();
+        assertTrue(title.contains("Automation"));
+
+        //4. Click on 'Signup / Login' button
+        driver.findElement(By.linkText("Signup / Login")).click();
+
+        //5. Verify 'New User Signup!' is visible
+        WebElement signUp = driver.findElement(By.xpath("(//h2)[3]"));
+        assertTrue(signUp.isDisplayed());
+
+        //6. Enter name and email address
+        driver.findElement(By.xpath("//input[@type='text']")).sendKeys("istanbul");
+        driver.findElement(By.xpath("//input[@data-qa='signup-email']")).sendKeys("blueMosque@hotmail.com");
+
+        //7. Click 'Signup' button
+        driver.findElement(By.xpath("(//button[@type='submit'])[2]")).click();
+
+        //8. Verify that 'ENTER ACCOUNT INFORMATION' is visible
+        WebElement yazi = driver.findElement(By.xpath("//b"));
+        assertTrue(yazi.isDisplayed());
+
+
+        //9. Fill details: Title, Name, Email, Password, Date of birth
+        driver.findElement(By.id("id_gender2")).click();
+        driver.findElement(By.id("password")).sendKeys("123456");
+        Actions actions = new Actions(driver);
+        actions.sendKeys(Keys.TAB).sendKeys("12")
+                .sendKeys(Keys.TAB).sendKeys("March").
+                sendKeys(Keys.TAB).sendKeys("2014").
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB)
+                //10. Fill details: First name, Last name, Company, Address, Address2, Country, State, City, Zipcode, Mobile Number
+                .sendKeys("Ankara").sendKeys(Keys.TAB)
+                .sendKeys("Adana").sendKeys(Keys.TAB)
+                .sendKeys("bireysel").sendKeys(Keys.TAB)
+                .sendKeys("yeni mah. 215. sok").sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).sendKeys("Canada").sendKeys(Keys.TAB).
+                sendKeys("Montreal").sendKeys(Keys.TAB).
+                sendKeys("Toronto").sendKeys(Keys.TAB).
+                sendKeys("437834789").sendKeys(Keys.TAB).
+                sendKeys("9832141783589").
+         //11. Click 'Create Account button'
+                        sendKeys(Keys.ENTER).perform();
+
+
+        //12. Verify that 'ACCOUNT CREATED!' is visible
+        WebElement kullanici = driver.findElement(By.xpath("//b"));
+        assertTrue(kullanici.isDisplayed());
+
+       //13. Click 'Continue' button
+        driver.findElement(By.xpath("//a[@data-qa='continue-button']")).click();
+
+       //14. Verify that 'Logged in as username' is visible
+        WebElement logged = driver.findElement(By.xpath("//i[@class='fa fa-user']"));
+        assertTrue(logged.isDisplayed());
+
+       //15. Click 'Delete Account' button
+        driver.findElement(By.xpath("//a[@href='/delete_account']")).click();
+
+       //16. Verify that 'ACCOUNT DELETED!' is visible and click 'Continue' button
+        WebElement delete = driver.findElement(By.xpath("//b"));
+        assertTrue(delete.isDisplayed());
+    }
+}
---
+++ src/test/java/utilities/TestBase.java
@@ -0,0 +1,107 @@
+package utilities;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+public abstract class TestBase {
+    //TestBase class'indan obje olusturmanin onune gecilmesi icin abstract yapilabilir
+//Orn: TestBase base=new TestBase();
+//Bu class'a extend ettigimiz test classlarinda ulasabiliriz.
+    protected static WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        wait(3);
+        driver.close();
+    }
+    //HARD WAIT METHOD
+    public static void wait(int second)  {
+        try {
+            Thread.sleep(second*1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    //ALERT ACCEPT
+    public static void alertAccept(){
+        driver.switchTo().alert().accept();
+    }
+    //ALERT DISMISS
+    public static void alertDismiss(){
+        driver.switchTo().alert().dismiss();
+    }
+    //ALERT GETTEXT()
+
+    public static void alertText(){
+        driver.switchTo().alert().getText();
+    }
+    //ALERT PROMPTBOX
+    public static void alertprompt(String text){
+        driver.switchTo().alert().sendKeys(text);
+    }
+    //DropDown VisibleText
+    /*
+        Select select2 = new Select(gun);
+        select2.selectByVisibleText("7");
+
+        //ddmVisibleText(gun,"7"); --> Yukarıdaki kullanım yerine sadece method ile handle edebilirim
+     */
+
+    public static void ddmVisibleText(WebElement ddm, String secenek){
+        Select select = new Select(ddm);
+        select.selectByVisibleText(secenek);
+    }
+    //DROPDOWN INDEX
+    public static void ddmIndex(WebElement ddm, int index){
+        Select select = new Select(ddm);
+        select.selectByIndex(index);
+    }
+    //DROPDOWN VALUE
+    public static void ddmValue(WebElement ddm,String secenek){
+        Select select= new Select(ddm);
+        select.selectByValue(secenek);
+    }
+    //SwitchTo:Sayfalar arasi gecis methodu
+    //Index 0 dan baslar
+    //Girilen indexteki windowHandle degerini alarak o sayfaya gecis yapar.
+    public static void switchToWindow(int sayfaIndexi){
+        List<String> windowHandleList = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(windowHandleList.get(sayfaIndexi));
+
+    }//SwitchToWindow2
+    public static void window(int sayi){
+        driver.switchTo().window(driver.getWindowHandles().toArray()[sayi].toString());
+    }
+    //AUTO COMPLETE REUSABLE METHOD
+    //THIS CODE IS USED FOR SELECTION AND VERIFYING OUR APP AUTO COMPLETE SEARCH FUNCTIONALITY
+    public static void searchAndSelectFromList(String keyword,String textFromList){
+
+        driver.findElement(By.id("myCountry")).sendKeys(keyword);
+        driver.findElement(By.xpath("//div[@id='myCountryautocomplete-list']//div[.='"+textFromList+"']")).click();
+        String result = driver.findElement(By.xpath("//p[@id='result']")).getText();
+        assertTrue(result.contains(textFromList));
+    }
+
+}
